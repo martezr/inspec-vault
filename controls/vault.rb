49bfe76cd80821f64b48a29e13e72496446bfd42
martezr <martez.reed@greenreedtech.com>
2018-07-26 09:02:28 -0500

    Add more controls

--- controls/vault.rb
+++ controls/vault.rb
@@ -25,6 +25,12 @@ vault_service = attribute(
   description: 'The name of the vault service'
 )
 
+vault_service_path = attribute(
+  'vault_service_path',
+  default: '/etc/systemd/system/vault.service',
+  description: 'The path on the system where the Vault service configuration file is located'
+)
+
 vault_dir = attribute(
   'vault_dir',
   default: '/opt/vault',
@@ -37,6 +43,18 @@ vault_user = attribute(
   description: 'The system user account that the vault service runs as'
 )
 
+vault_tlscert = attribute(
+  'vault_tlscert',
+  description: 'Path to TLS certificate file',
+  default: '/opt/vault/ssl/server_cert.pem'
+)
+
+vault_tlskey = attribute(
+  'vault_tlskey',
+  description: 'Path to TLS key file',
+  default: '/opt/vaul/ssl/server_key.pem'
+)
+
 # check if vault exists
 only_if do
   command('vault').exist?
@@ -44,7 +62,11 @@ end
 
 control 'vault-1.1' do
   impact 1.0
-  title 'Ensure Vault is upgrade frequently'
+  title 'Keep Vault up to date'
+  desc 'Vault is actively developed, and updating frequently is important to incorporate security fixes
+  and any changes in default settings such as key lengths or cipher suites.'
+
+  ref 'Vault Production Hardening', url: 'https://www.vaultproject.io/guides/operations/production.html'
 
   describe vault_version do
     its('version') { should cmp >= 'v0.10.1' }
@@ -52,6 +74,17 @@ control 'vault-1.1' do
 end
 
 control 'vault-1.2' do
+  impact 1.0
+  title 'Audit Vault executable'
+  desc 'Audit all Vault executable activities'
+
+  only_if { os.linux? }
+  describe auditd do
+    its(:lines) { should include('-w /usr/local/bin/vault -p rwxa -k vault') }
+  end
+end
+
+control 'vault-1.3' do
   impact 1.0
   title 'Secure Vault configuration files'
 
@@ -63,24 +96,82 @@ control 'vault-1.2' do
   end
 end
 
-describe vault_audit_file do
-  its('status') { should eq('enabled') }
-  its('path') { should eq('/tmp/example-file.txt') }
+control 'vault-1.4' do
+  impact 1.0
+  title 'Audit Vault files and directories'
+
+  only_if { os.linux? }
+  describe auditd do
+    its(:lines) { should include('-w /opt/vault/ -p rwxa -k vault') }
+  end
 end
 
-describe directory(vault_dir) do
-  its('owner') { should eq vault_user }
-  it { should_not be_readable.by('others') }
-  it { should_not be_writable.by('others') }
-  it { should_not be_executable.by('others') }
+control 'vault-1.5' do
+  impact 1.0
+  title 'Audit Vault service configuration'
+
+  only_if { os.linux? }
+  rule = '-w ' + vault_service_path + ' -p rwxa -k vault'
+  describe auditd do
+    its(:lines) { should include(rule) }
+  end
 end
 
-describe service(vault_service) do
-  it { should be_installed }
-  it { should be_enabled }
-  it { should be_running }
+control 'vault-1.6' do
+  impact 1.0
+  title 'Ensure that the vault service is running'
+
+  describe service(vault_service) do
+    it { should be_installed }
+    it { should be_enabled }
+    it { should be_running }
+  end
 end
 
-describe auditd do
-  its(:lines) { should include('-w /opt/vault/config -p rwxa -k vault') }
+control 'vault-1.7' do
+  impact 1.0
+  title 'Ensure Vault is not running as root'
+  desc 'Ensure that the Vault service is not being run as root'
+
+  describe processes('vault') do
+    its('users') { should_not eq ['root'] }
+  end
+end
+
+control 'vault-1.8' do
+  impact 1.0
+  title 'Ensure swap is disabled on the system'
+  desc 'Ensure that swap is disabled on the system to prevent secrets from being written to disk'
+
+  describe command('swapon -s | grep -v Filename') do
+    its('exit_status') { should eq 1 }
+  end
+end
+
+control 'vault-1.9' do
+  impact 1.0
+  title 'Ensure Vault audit logging is enabled'
+
+  describe vault_audit_file do
+    its('status') { should eq('enabled') }
+    its('path') { should eq('/tmp/example-file.txt') }
+  end
+end
+
+control 'vault-1.10' do
+  impact 1.0
+  title 'Verify that vault.service file permissions are set to 644 or more restrictive'
+  desc 'Verify that the \'vault.service\' file permissions are correctly set to \'644\' or more restrictive.'
+
+  describe file(vault_service_path) do
+    it { should exist }
+    it { should be_file }
+    it { should be_readable.by('owner') }
+    it { should be_writable.by('owner') }
+    it { should be_readable.by('group') }
+    it { should_not be_writable.by('group') }
+    it { should be_readable.by('other') }
+    it { should_not be_writable.by('other') }
+    it { should_not be_executable }
+  end
 end
